fc88de7a85070dad7035ad6fea4484de2ccd3812
Maddy2312 <madhav2580sharma@gmail.com>
2025-01-10 22:05:19 +1100

    initial

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+require("dotenv").config()
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const connectDB = require("./config/mongodb");
 const userRoutes = require("./routes/user.routes");
 const indexRoutes = require("./routes/index.routes");
+app.use(cors());
 
-require("dotenv").config()
 connectDB();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
